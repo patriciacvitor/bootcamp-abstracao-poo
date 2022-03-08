f37f5eadd61a12a0a08e4d949d18b47835a86222
Patrícia Vitor <82560461+patriciacvitor@users.noreply.github.com>
2022-03-08 15:17:44 -0300

    changes in files

---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,8 @@
+package br.com.dio.desafio.dominio;
+
+public class Dev {
+
+    private String nome;
+    private Set<Conteudo> conteudosInscritos;
+    private Set<Conteudo> conteudosConcluidos;
+}
