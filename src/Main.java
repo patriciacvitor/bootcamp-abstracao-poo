be5eb36e453ebde53302f90fc5f3d7c59ec1b77c
Patrícia Vitor <82560461+patriciacvitor@users.noreply.github.com>
2022-03-08 20:20:04 -0300

    changes in files

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -24,8 +26,40 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
+        /*
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+         */
+
+        Bootcamp bootcamp1 = new Bootcamp();
+        bootcamp1.setNome("Bootcamp Java Developer");
+        bootcamp1.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp1.getConteudos().add(curso1);
+        bootcamp1.getConteudos().add(curso2);
+        bootcamp1.getConteudos().add(mentoria);
+
+        Dev devPatricia = new Dev();
+        devPatricia.setNome("Patrícia");
+        devPatricia.inscreverBootcamp(bootcamp1);
+        System.out.println("Conteúdos inscritos Patrícia: " + devPatricia.getConteudosInscritos());
+        devPatricia.progredir();
+        devPatricia.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Patrícia: " + devPatricia.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Patrícia: " + devPatricia.getConteudosConcluidos());
+        System.out.println("XP Patrícia: " + devPatricia.calcularTotalXp());
+
+        System.out.println("----------------------------------------------------------------");
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp1);
+        System.out.println("Conteúdos inscritos Camila: " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos inscritos Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos concluídos Camila: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP Camila: " + devCamila.calcularTotalXp());
+
     }
 }
--- src/br/com/dio/desafio/dominio/Bootcamp.java
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -1,5 +1,11 @@
 package br.com.dio.desafio.dominio;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class Bootcamp {
 
     private String nome;
